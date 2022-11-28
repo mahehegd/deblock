0c6ff47fd1c12bd6ab13dd87832abcde395c3bde
mahehegd <mahesh.hegde129@gmail.com>
2022-11-28 18:08:03 +0000

    Use Object mapper with default configuration

---
+++ src/main/java/org/deblock/exercise/configuration/JacksonConfiguration.java
@@ -0,0 +1,20 @@
+package org.deblock.exercise.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+
+        return mapper;
+    }
+}
--- src/test/java/org/deblock/exercise/controller/DeblockFlightControllerIntegrationTest.java
+++ src/test/java/org/deblock/exercise/controller/DeblockFlightControllerIntegrationTest.java
@@ -28,88 +28,95 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(DeblockFlightController.class)
 public class DeblockFlightControllerIntegrationTest {
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private DeblockFlightsService service;
+        @Autowired
+        ObjectMapper objectMapper;
 
-    @Test
-    public void testHome() throws Exception {
-        String uri = "/";
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        @MockBean
+        private DeblockFlightsService service;
 
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
-        String content = mvcResult.getResponse().getContentAsString();
-        assertEquals(content, "Hi deblockFlights!");
-    }
+        @Test
+        public void testHome() throws Exception {
+                String uri = "/";
+                MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)
+                                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
-    @Test
-    public void testGetFlights() throws Exception {
-        String uri = "/v1/flights";
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.findAndRegisterModules();
+                int status = mvcResult.getResponse().getStatus();
+                assertEquals(200, status);
+                String content = mvcResult.getResponse().getContentAsString();
+                assertEquals(content, "Hi deblockFlights!");
+        }
 
-        List<SearchResponseParam> response = new ArrayList<>();
-        SearchResponseParam responseParam = new SearchResponseParam("CrazyAir", Suppliers.crazyAir, 100.00, "LHR",
-                "AMS", LocalDate.now(), LocalDate.now().plusDays(2));
-        response.add(responseParam);
+        @Test
+        public void testGetFlights() throws Exception {
+                String uri = "/v1/flights";
 
-        Mockito.when(service.getFlights(Mockito.any())).thenReturn(response);
+                objectMapper.findAndRegisterModules();
 
-        SearchRequestParam request = new SearchRequestParam("LHR", "AMS", LocalDate.now(), LocalDate.now().plusDays(2),
-                2);
+                List<SearchResponseParam> response = new ArrayList<>();
+                SearchResponseParam responseParam = new SearchResponseParam("CrazyAir", Suppliers.crazyAir, 100.00,
+                                "LHR",
+                                "AMS", LocalDate.now(), LocalDate.now().plusDays(2));
+                response.add(responseParam);
 
-        String inputJson = objectMapper.writeValueAsString(request);
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
-                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+                Mockito.when(service.getFlights(Mockito.any())).thenReturn(response);
 
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
-        String content = mvcResult.getResponse().getContentAsString();
-        List<SearchResponseParam> result = objectMapper.readValue(content,
-                new TypeReference<List<SearchResponseParam>>() {
-                });
-        assertEquals(result.size(), 1);
-    }
+                SearchRequestParam request = new SearchRequestParam("LHR", "AMS", LocalDate.now(),
+                                LocalDate.now().plusDays(2),
+                                2);
 
-    @Test
-    public void testGetFlightsForInvalidDepartureDate() throws Exception {
-        String uri = "/v1/flights";
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.findAndRegisterModules();
+                String inputJson = objectMapper.writeValueAsString(request);
+                MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
 
-        SearchRequestParam request = new SearchRequestParam("LHR", "AMS", LocalDate.now().minusDays(1), LocalDate.now().plusDays(2),
-                2);
+                int status = mvcResult.getResponse().getStatus();
+                assertEquals(200, status);
+                String content = mvcResult.getResponse().getContentAsString();
+                List<SearchResponseParam> result = objectMapper.readValue(content,
+                                new TypeReference<List<SearchResponseParam>>() {
+                                });
+                assertEquals(result.size(), 1);
+        }
 
-        String inputJson = objectMapper.writeValueAsString(request);
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
-                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+        @Test
+        public void testGetFlightsForInvalidDepartureDate() throws Exception {
+                String uri = "/v1/flights";
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.findAndRegisterModules();
 
-        int status = mvcResult.getResponse().getStatus();
-        Exception exception = mvcResult.getResolvedException();
-        assertEquals(400, status);
-        assertInstanceOf(MethodArgumentNotValidException.class, exception);
-    }
+                SearchRequestParam request = new SearchRequestParam("LHR", "AMS", LocalDate.now().minusDays(1),
+                                LocalDate.now().plusDays(2),
+                                2);
 
-    @Test
-    public void testGetFlightsForInvalidOrigin() throws Exception {
-        String uri = "/v1/flights";
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.findAndRegisterModules();
+                String inputJson = objectMapper.writeValueAsString(request);
+                MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
 
-        SearchRequestParam request = new SearchRequestParam("LH", "AMS", LocalDate.now(), LocalDate.now().plusDays(2),
-                2);
+                int status = mvcResult.getResponse().getStatus();
+                Exception exception = mvcResult.getResolvedException();
+                assertEquals(400, status);
+                assertInstanceOf(MethodArgumentNotValidException.class, exception);
+        }
 
-        String inputJson = objectMapper.writeValueAsString(request);
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
-                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+        @Test
+        public void testGetFlightsForInvalidOrigin() throws Exception {
+                String uri = "/v1/flights";
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.findAndRegisterModules();
 
-        int status = mvcResult.getResponse().getStatus();
-        String message = mvcResult.getResolvedException().getMessage();
-        assertEquals(400, status);
-        assertTrue(message.contains("Origin should be 3 character long"));
-    }
+                SearchRequestParam request = new SearchRequestParam("LH", "AMS", LocalDate.now(),
+                                LocalDate.now().plusDays(2),
+                                2);
+
+                String inputJson = objectMapper.writeValueAsString(request);
+                MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+                int status = mvcResult.getResponse().getStatus();
+                String message = mvcResult.getResolvedException().getMessage();
+                assertEquals(400, status);
+                assertTrue(message.contains("Origin should be 3 character long"));
+        }
 }
